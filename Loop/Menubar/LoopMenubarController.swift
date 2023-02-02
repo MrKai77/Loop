6fe7a0d6cf02b8a8f0929783bc603bd893596a73
Kai Azim <shotasushi77@gmail.com>
2023-02-02 08:42:13 -0700

    ✨ Use for loop to show resize options in menubar item

--- Loop/Menubar/LoopMenubarController.swift
+++ Loop/Menubar/LoopMenubarController.swift
@@ -6,44 +6,62 @@
 //
 
 import Cocoa
+import KeyboardShortcuts
+
+struct resizeWindowMenuItem {
+    var divider: Bool?
+    var title: String?
+    var selector: Selector?
+    var shortcut: KeyboardShortcuts.Name?
+}
 
 class LoopMenubarController {
+    
+    let resizeWindowMenuItems = [
+        resizeWindowMenuItem(title: "Maximize", selector: #selector(resizeWindowMaximize), shortcut: .resizeMaximize),
+        resizeWindowMenuItem(divider: true),
+        resizeWindowMenuItem(title: "Top Half", selector: #selector(resizeWindowTopHalf), shortcut: .resizeTopHalf),
+        resizeWindowMenuItem(title: "Bottom Half", selector: #selector(resizeWindowBottomHalf), shortcut: .resizeBottomHalf),
+        resizeWindowMenuItem(title: "Right Half", selector: #selector(resizeWindowRightHalf), shortcut: .resizeRightHalf),
+        resizeWindowMenuItem(title: "Left Half", selector: #selector(resizeWindowLeftHalf), shortcut: .resizeLeftHalf),
+        resizeWindowMenuItem(divider: true),
+        resizeWindowMenuItem(title: "Top Right Quarter", selector: #selector(resizeWindowTopRightQuarter), shortcut: .resizeTopRightQuarter),
+        resizeWindowMenuItem(title: "Top Left Quarter", selector: #selector(resizeWindowTopLeftQuarter), shortcut: .resizeTopLeftQuarter),
+        resizeWindowMenuItem(title: "Bottom Right Quarter", selector: #selector(resizeWindowBottomRightQuarter), shortcut: .resizeBottomRightQuarter),
+        resizeWindowMenuItem(title: "Bottom Left Quarter", selector: #selector(resizeWindowBottomLeftQuarter), shortcut: .resizeBottomLeftQuarter),
+        resizeWindowMenuItem(divider: true),
+        resizeWindowMenuItem(title: "Right Third", selector: #selector(resizeWindowRightThird), shortcut: .resizeRightThird),
+        resizeWindowMenuItem(title: "Right Two Thirds", selector: #selector(resizeWindowRightTwoThirds), shortcut: .resizeRightTwoThirds),
+        resizeWindowMenuItem(title: "Center Third", selector: #selector(resizeWindowRLCenterThird), shortcut: .resizeRLCenterThird),
+        resizeWindowMenuItem(title: "Left Two Thirds", selector: #selector(resizeWindowLeftTwoThirds), shortcut: .resizeLeftTwoThirds),
+        resizeWindowMenuItem(title: "Left Third", selector: #selector(resizeWindowLeftThird), shortcut: .resizeLeftThird)
+    ]
     
     let windowResizer = WindowResizer()
     private var statusItem: NSStatusItem!
     
     func show() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        if let button = statusItem.button {
-            button.image = NSImage(named: NSImage.Name("menubarIcon"))
-        }
+        guard let button = statusItem.button else { return }
+        button.image = NSImage(named: NSImage.Name("menubarIcon"))
+        
+        let loopMenu = NSMenu()
         
         let resizeWindow = NSMenuItem(title: "Resize Window", action: nil, keyEquivalent: "")
         resizeWindow.submenu = NSMenu()
         
-        resizeWindow.submenu?.addItem(withTitle: "Maximize", action: #selector(resizeWindowMaximize), keyEquivalent: "").setShortcut(for: .resizeMaximize)
-        
-        resizeWindow.submenu?.addItem(NSMenuItem.separator())
-        
-        resizeWindow.submenu?.addItem(withTitle: "Top", action: #selector(resizeWindowTopHalf), keyEquivalent: "").setShortcut(for: .resizeTopHalf)
-        resizeWindow.submenu?.addItem(withTitle: "Bottom", action: #selector(resizeWindowBottomHalf), keyEquivalent: "").setShortcut(for: .resizeBottomHalf)
-        resizeWindow.submenu?.addItem(withTitle: "Right", action: #selector(resizeWindowRightHalf), keyEquivalent: "").setShortcut(for: .resizeRightHalf)
-        resizeWindow.submenu?.addItem(withTitle: "Left", action: #selector(resizeWindowLeftHalf), keyEquivalent: "").setShortcut(for: .resizeLeftHalf)
-        
-        resizeWindow.submenu?.addItem(NSMenuItem.separator())
-        
-        resizeWindow.submenu?.addItem(withTitle: "Top Right", action: #selector(resizeWindowTopRightQuarter), keyEquivalent: "").setShortcut(for: .resizeTopRightQuarter)
-        resizeWindow.submenu?.addItem(withTitle: "Top Left", action: #selector(resizeWindowTopLeftQuarter), keyEquivalent: "").setShortcut(for: .resizeTopLeftQuarter)
-        resizeWindow.submenu?.addItem(withTitle: "Bottom Right", action: #selector(resizeWindowBottomRightQuarter), keyEquivalent: "").setShortcut(for: .resizeBottomRightQuarter)
-        resizeWindow.submenu?.addItem(withTitle: "Bottom Left", action: #selector(resizeWindowBottomLeftQuarter), keyEquivalent: "").setShortcut(for: .resizeBottomLeftQuarter)
-        
-        if let items = resizeWindow.submenu?.items {
-            for item in items {
-                item.target = self
+        for item in resizeWindowMenuItems {
+            if item.divider != nil {
+                resizeWindow.submenu?.addItem(NSMenuItem.separator())
+            } else {
+                let menuItem = NSMenuItem(title: item.title!, action: item.selector, keyEquivalent: "")
+                menuItem.setShortcut(for: item.shortcut)
+                menuItem.target = self
+                
+                resizeWindow.submenu?.addItem(menuItem)
             }
         }
         
-        let loopMenu = NSMenu()
         loopMenu.addItem(resizeWindow)
         loopMenu.addItem(NSMenuItem.separator())
         if #available(macOS 13, *) {
@@ -84,6 +102,22 @@ class LoopMenubarController {
     }
     @objc func resizeWindowBottomLeftQuarter() {
         self.windowResizer.resizeFrontmostWindowWithDirection(.bottomLeftQuarter)
+    }
+    
+    @objc func resizeWindowRightThird() {
+        self.windowResizer.resizeFrontmostWindowWithDirection(.rightThird)
+    }
+    @objc func resizeWindowRightTwoThirds() {
+        self.windowResizer.resizeFrontmostWindowWithDirection(.rightTwoThirds)
+    }
+    @objc func resizeWindowRLCenterThird() {
+        self.windowResizer.resizeFrontmostWindowWithDirection(.RLcenterThird)
+    }
+    @objc func resizeWindowLeftTwoThirds() {
+        self.windowResizer.resizeFrontmostWindowWithDirection(.leftTwoThirds)
+    }
+    @objc func resizeWindowLeftThird() {
+        self.windowResizer.resizeFrontmostWindowWithDirection(.leftThird)
     }
     
     @objc func openSettings() {
