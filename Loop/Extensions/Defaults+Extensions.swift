3d784b2b113f6cc7a6b9d3b7ed88e4f4ea719df0
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2023-08-18 18:13:59 -0600

    ✨ Use `CGKeyCode` instead of `UInt16`

--- Loop/Extensions/Defaults+Extensions.swift
+++ Loop/Extensions/Defaults+Extensions.swift
@@ -38,63 +38,63 @@ extension Defaults.Keys {
     )
 
     // Halves
-    static let topHalfKeybind = Key<[Set<UInt16>]>(
+    static let topHalfKeybind = Key<[Set<CGKeyCode>]>(
         "topHalfKeybind",
         default: [[.kVK_ANSI_W], [.kVK_UpArrow]]
     )
-    static let bottomHalfKeybind = Key<[Set<UInt16>]>(
+    static let bottomHalfKeybind = Key<[Set<CGKeyCode>]>(
         "bottomHalfKeybind",
         default: [[.kVK_ANSI_S], [.kVK_DownArrow]]
     )
-    static let rightHalfKeybind = Key<[Set<UInt16>]>(
+    static let rightHalfKeybind = Key<[Set<CGKeyCode>]>(
         "rightHalfKeybind",
         default: [[.kVK_ANSI_D], [.kVK_RightArrow]]
     )
-    static let leftHalfKeybind = Key<[Set<UInt16>]>(
+    static let leftHalfKeybind = Key<[Set<CGKeyCode>]>(
         "leftHalfKeybind",
         default: [[.kVK_ANSI_A], [.kVK_LeftArrow]]
     )
 
     // Quarters
-    static let topLeftQuarter = Key<[Set<UInt16>]>(
+    static let topLeftQuarter = Key<[Set<CGKeyCode>]>(
         "topLeftQuarter",
         default: [[.kVK_ANSI_W, .kVK_ANSI_A],
                   [.kVK_UpArrow, .kVK_LeftArrow]]
     )
-    static let topRightQuarter = Key<[Set<UInt16>]>(
+    static let topRightQuarter = Key<[Set<CGKeyCode>]>(
         "topRightQuarter",
         default: [[.kVK_ANSI_W, .kVK_ANSI_D],
                   [.kVK_UpArrow, .kVK_RightArrow]]
     )
-    static let bottomRightQuarter = Key<[Set<UInt16>]>(
+    static let bottomRightQuarter = Key<[Set<CGKeyCode>]>(
         "bottomRightQuarter",
         default: [[.kVK_ANSI_S, .kVK_ANSI_D],
                   [.kVK_DownArrow, .kVK_RightArrow]]
     )
-    static let bottomLeftQuarter = Key<[Set<UInt16>]>(
+    static let bottomLeftQuarter = Key<[Set<CGKeyCode>]>(
         "bottomLeftQuarter",
         default: [[.kVK_ANSI_S, .kVK_ANSI_A],
                   [.kVK_DownArrow, .kVK_LeftArrow]]
     )
 
     // Thirds
-    static let leftThird = Key<[Set<UInt16>]>(
+    static let leftThird = Key<[Set<CGKeyCode>]>(
         "leftThird",
         default: [[.kVK_ANSI_J]]
     )
-    static let leftTwoThirds = Key<[Set<UInt16>]>(
+    static let leftTwoThirds = Key<[Set<CGKeyCode>]>(
         "leftTwoThirds",
         default: [[.kVK_ANSI_U]]
     )
-    static let horizontalCenterThird = Key<[Set<UInt16>]>(
+    static let horizontalCenterThird = Key<[Set<CGKeyCode>]>(
         "horizontalCenterThird",
         default: [[.kVK_ANSI_K]]
     )
-    static let rightTwoThirds = Key<[Set<UInt16>]>(
+    static let rightTwoThirds = Key<[Set<CGKeyCode>]>(
         "rightTwoThirds",
         default: [[.kVK_ANSI_O]]
     )
-    static let rightThird = Key<[Set<UInt16>]>(
+    static let rightThird = Key<[Set<CGKeyCode>]>(
         "rightThird",
         default: [[.kVK_ANSI_L]]
     )
