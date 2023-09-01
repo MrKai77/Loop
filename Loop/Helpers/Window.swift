d2c08794216b01a75f64cb15f3754152a47249df
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2023-09-01 16:51:05 -0600

    ✨ Rename the `AXUIElement` inside `Window` to `axWindow`

--- Loop/Helpers/Window.swift
+++ Loop/Helpers/Window.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 class Window {
     private let kAXFullscreenAttribute = "AXFullScreen"
-    let window: AXUIElement
+    let axWindow: AXUIElement
 
     init?(window: AXUIElement) {
-        self.window = window
+        self.axWindow = window
 
         if role != kAXWindowRole,
            subrole != kAXStandardWindowSubrole {
@@ -22,39 +22,39 @@ class Window {
 
     convenience init?(pid: pid_t) {
         let element = AXUIElementCreateApplication(pid)
-        guard let window = element.copyAttributeValue(attribute: kAXFocusedWindowAttribute) else { return nil }
+        guard let window = element.getValue(attribute: kAXFocusedWindowAttribute) else { return nil }
         // swiftlint:disable force_cast
         self.init(window: window as! AXUIElement)
         // swiftlint:enable force_cast
     }
 
     var role: String? {
-        return self.window.copyAttributeValue(attribute: kAXRoleAttribute) as? String
+        return self.axWindow.getValue(attribute: kAXRoleAttribute) as? String
     }
 
     var subrole: String? {
-        return self.window.copyAttributeValue(attribute: kAXSubroleAttribute) as? String
+        return self.axWindow.getValue(attribute: kAXSubroleAttribute) as? String
     }
 
     var isFullscreen: Bool {
-        let result = self.window.copyAttributeValue(attribute: kAXFullscreenAttribute) as? NSNumber
+        let result = self.axWindow.getValue(attribute: kAXFullscreenAttribute) as? NSNumber
         return result?.boolValue ?? false
     }
     @discardableResult
     func setFullscreen(_ state: Bool) -> Bool {
-        return self.window.setAttributeValue(
+        return self.axWindow.setValue(
             attribute: kAXFullscreenAttribute,
             value: state ? kCFBooleanTrue : kCFBooleanFalse
         )
     }
 
     var isMinimized: Bool {
-        let result = self.window.copyAttributeValue(attribute: kAXMinimizedAttribute) as? NSNumber
+        let result = self.axWindow.getValue(attribute: kAXMinimizedAttribute) as? NSNumber
         return result?.boolValue ?? false
     }
     @discardableResult
     func setMinimized(_ state: Bool) -> Bool {
-        return self.window.setAttributeValue(
+        return self.axWindow.setValue(
             attribute: kAXMinimizedAttribute,
             value: state ? kCFBooleanTrue : kCFBooleanFalse
         )
@@ -62,7 +62,7 @@ class Window {
 
     var origin: CGPoint {
         var point: CGPoint = .zero
-        guard let value = self.window.copyAttributeValue(attribute: kAXPositionAttribute) else { return point }
+        guard let value = self.axWindow.getValue(attribute: kAXPositionAttribute) else { return point }
         // swiftlint:disable force_cast
         AXValueGetValue(value as! AXValue, .cgPoint, &point)    // Convert to CGPoint
         // swiftlint:enable force_cast
@@ -72,14 +72,14 @@ class Window {
     func setOrigin(_ origin: CGPoint) -> Bool {
         var position = origin
         if let value = AXValueCreate(AXValueType.cgPoint, &position) {
-            return self.window.setAttributeValue(attribute: kAXPositionAttribute, value: value)
+            return self.axWindow.setValue(attribute: kAXPositionAttribute, value: value)
         }
         return false
     }
 
     var size: CGSize {
         var size: CGSize = .zero
-        guard let value = self.window.copyAttributeValue(attribute: kAXSizeAttribute) else { return size }
+        guard let value = self.axWindow.getValue(attribute: kAXSizeAttribute) else { return size }
         // swiftlint:disable force_cast
         AXValueGetValue(value as! AXValue, .cgSize, &size)      // Convert to CGSize
         // swiftlint:enable force_cast
@@ -89,7 +89,7 @@ class Window {
     func setSize(_ size: CGSize) -> Bool {
         var size = size
         if let value = AXValueCreate(AXValueType.cgSize, &size) {
-            return self.window.setAttributeValue(attribute: kAXSizeAttribute, value: value)
+            return self.axWindow.setValue(attribute: kAXSizeAttribute, value: value)
         }
         return false
     }
