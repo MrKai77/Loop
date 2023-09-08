fa716268f7ca7d3d8fb5563ab5655c03478579b9
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2023-09-07 18:27:47 -0600

    ✨ #25 Ability to add trigger delay

--- Loop/Extensions/Defaults+Extensions.swift
+++ Loop/Extensions/Defaults+Extensions.swift
@@ -20,6 +20,7 @@ extension Defaults.Keys {
     static let gradientColor = Key<Color>("gradientColor", default: Color(.black))
 
     static let triggerKey = Key<CGKeyCode>("triggerKey", default: .kVK_Function)
+    static let triggerDelay = Key<Float>("triggerDelay", default: 0)
     static let radialMenuCornerRadius = Key<CGFloat>("radialMenuCornerRadius", default: 50)
     static let radialMenuThickness = Key<CGFloat>("radialMenuThickness", default: 22)
 
