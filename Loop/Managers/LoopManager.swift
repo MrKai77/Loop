396bb3eb85fde505eb5945b5e31f84139e354ed4
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2024-02-15 16:59:30 -0700

    🐛 Fix Loop closing when other modifiers are pressed

--- Loop/Managers/LoopManager.swift
+++ Loop/Managers/LoopManager.swift
@@ -288,8 +288,7 @@ class LoopManager: ObservableObject {
         self.triggerDelayTimer = nil
         processModifiers(event)
 
-        // Why sort the set? I have no idea. But it works much more reliably when sorted!
-        if self.currentlyPressedModifiers.sorted().contains(Defaults[.triggerKey].sorted()) {
+        if Defaults[.triggerKey].isSubset(of: self.currentlyPressedModifiers) {
             let useTriggerDelay = Defaults[.triggerDelay] > 0.1
             let useDoubleClickTrigger = Defaults[.doubleClickToTrigger]
 
@@ -317,8 +316,6 @@ class LoopManager: ObservableObject {
         } else {
             self.currentlyPressedModifiers.insert(event.keyCode)
         }
-
-        print("Current modifiers: \(currentlyPressedModifiers)")
     }
 
     private func openLoop() {
