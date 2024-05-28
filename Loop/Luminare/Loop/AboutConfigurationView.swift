5a2fe5fcf5c758d2fe9d6979b3369ec7dfa5e441
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2024-05-28 17:38:27 -0600

    ✨ Add credits

--- Loop/Luminare/Loop/AboutConfigurationView.swift
+++ Loop/Luminare/Loop/AboutConfigurationView.swift
@@ -17,6 +17,49 @@ struct AboutConfigurationView: View {
 
     @StateObject private var updater = SoftwareUpdater()
 
+    let credits: [CreditItem] = [
+        .init(
+            "Kai",
+            "Development",
+            .init(string: "https://github.com/mrkai77")!,
+            avatar: .init(string: "https://github.com/mrkai77.png?size=200")!
+        ),
+        .init(
+            "Jace",
+            "Design",
+            .init(string: "https://x.com/jacethings")!,
+            avatar: .init(string: "https://github.com/soft-bred.png?size=200")!
+        ),
+        .init(
+            "Kami",
+            "Development support",
+            .init(string: "https://github.com/senpaihunters")!,
+            avatar: .init(string: "https://github.com/senpaihunters.png?size=200")!
+        ),
+        .init(
+            "Greg Lassale",
+            "Icon contributor",
+            .init(string: "https://x.com/greglassale")!,
+            avatar: .init(string: "https://pbs.twimg.com/profile_images/1746348765127094272/eNO2LxOQ_200x200.jpg")!
+        )
+    ]
+
+    struct CreditItem: Identifiable {
+        var id: String { name }
+
+        let name: String
+        let description: LocalizedStringKey
+        let url: URL
+        let avatar: URL
+
+        init(_ name: String, _ description: LocalizedStringKey, _ url: URL, avatar: URL) {
+            self.name = name
+            self.description = description
+            self.avatar = avatar
+            self.url = url
+        }
+    }
+
     var body: some View {
         LuminareSection {
             HStack {
@@ -74,5 +117,57 @@ struct AboutConfigurationView: View {
             }
             .padding(8)
         }
+
+        LuminareSection("Credits") {
+            ForEach(credits) { credit in
+                creditsView(credit)
+            }
+        }
+
+        Button {
+            openURL(URL(string: "https://github.com/MrKai77/Loop/graphs/contributors")!)
+        } label: {
+            Text("…and all the awesome open source contributors on GitHub!", comment: "End of credits in about tab")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+
+    @ViewBuilder
+    func creditsView(_ credit: CreditItem) -> some View {
+        Button {
+            openURL(credit.url)
+        } label: {
+            HStack(spacing: 8) {
+                AsyncImage(url: credit.avatar) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } placeholder: {
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .foregroundStyle(.tertiary)
+                        .aspectRatio(contentMode: .fit)
+                }
+                .frame(height: 40)
+                .overlay {
+                    Circle()
+                        .strokeBorder(.white.opacity(0.1), lineWidth: 1)
+                }
+                .clipShape(.circle)
+
+                VStack(alignment: .leading) {
+                    Text(credit.name)
+                    Text(credit.description)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+
+                Spacer()
+            }
+            .padding(8)
+        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
