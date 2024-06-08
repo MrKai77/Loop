9ce5818bf3244d97d77c67f24faa593ffec3b6ab
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2024-06-07 21:57:47 -0600

    🐛 Remove force unwrap

--- Loop/Managers/LoopManager.swift
+++ Loop/Managers/LoopManager.swift
@@ -223,10 +223,11 @@ class LoopManager: ObservableObject {
                 changeAction(action, triggeredFromScreenChange: true)
             } else {
                 if let screenToResizeOn,
+                   let window = targetWindow,
                    !Defaults[.previewVisibility] {
                     performHapticFeedback()
                     WindowEngine.resize(
-                        targetWindow!,
+                        window,
                         to: currentAction,
                         on: screenToResizeOn
                     )
@@ -250,9 +251,10 @@ class LoopManager: ObservableObject {
                 Notification.Name.updateUIDirection.post(userInfo: ["action": self.currentAction])
 
                 if let screenToResizeOn = self.screenToResizeOn,
+                   let window = self.targetWindow,
                    !Defaults[.previewVisibility] {
                     WindowEngine.resize(
-                        self.targetWindow!,
+                        window,
                         to: self.currentAction,
                         on: screenToResizeOn
                     )
