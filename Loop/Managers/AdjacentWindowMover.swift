4ba189b9bbc6554d2d3e85baaa489ca50806eb51
Kai Azim <hello@mrkai77.dev>
2024-08-31 16:48:41 -0600

    🐞 Only adjust other axis when <20 pt difference

--- Loop/Managers/AdjacentWindowMover.swift
+++ Loop/Managers/AdjacentWindowMover.swift
@@ -32,7 +32,6 @@ class AdjacentWindowMover {
                 self.handler()
             } else {
                 self.mainWindow = nil
-
                 self.topAdjacentWindows = []
                 self.bottomAdjacentWindows = []
                 self.leadingAdjacentWindows = []
@@ -62,15 +61,19 @@ class AdjacentWindowMover {
 }
 
 private extension AdjacentWindowMover {
-    class AjacentWindowState: Equatable, Hashable {
+    struct AjacentWindowState: Equatable, Hashable {
         let window: Window
         let initialFrame: CGRect
 
-        var preview: PreviewController?
+        var shouldAdjustWidth: Bool
+        var shouldAdjustHeight: Bool
 
-        init(_ window: Window) {
+        init(_ window: Window, initialFrame: CGRect, shouldAlsoAdjustWidth: Bool = false, shouldAlsoAdjustHeight: Bool = false) {
             self.window = window
-            self.initialFrame = window.frame
+            self.initialFrame = initialFrame
+
+            self.shouldAdjustHeight = shouldAlsoAdjustHeight
+            self.shouldAdjustWidth = shouldAlsoAdjustWidth
         }
 
         func hash(into hasher: inout Hasher) {
@@ -92,69 +95,85 @@ private extension AdjacentWindowMover {
 
         // Top
         let topWindows = windows.filter { $0.frame.maxY.approximatelyEquals(to: frame.minY, tolerance: 100) }
-        for target in topWindows where target.cgWindowID != window.cgWindowID && screenFrame.contains(target.frame) && !topAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
-            topAdjacentWindows.append(.init(target))
+        for target in topWindows where target.cgWindowID != window.cgWindowID && !topAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
+            let itemFrame = target.frame
+            if screenFrame.contains(itemFrame) {
+                let shouldAdjustWidth = abs(itemFrame.minX - frame.minX) < 20 && abs(itemFrame.maxX - frame.maxX) < 20
+                topAdjacentWindows.append(.init(target, initialFrame: itemFrame, shouldAlsoAdjustWidth: shouldAdjustWidth))
+            }
         }
 
         // Bottom
         let bottomWindows = windows.filter { $0.frame.minY.approximatelyEquals(to: frame.maxY, tolerance: 100) }
-        for target in bottomWindows where target.cgWindowID != window.cgWindowID && screenFrame.contains(target.frame) && !bottomAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
-            bottomAdjacentWindows.append(.init(target))
+        for target in bottomWindows where target.cgWindowID != window.cgWindowID && !bottomAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
+            let itemFrame = target.frame
+            if screenFrame.contains(itemFrame) {
+                let shouldAdjustWidth = abs(itemFrame.minX - frame.minX) < 20 && abs(itemFrame.maxX - frame.maxX) < 20
+                bottomAdjacentWindows.append(.init(target, initialFrame: itemFrame, shouldAlsoAdjustWidth: shouldAdjustWidth))
+            }
         }
 
         // Leading
         let leadingWindows = windows.filter { $0.frame.maxX.approximatelyEquals(to: frame.minX, tolerance: 100) }
-        for target in leadingWindows where target.cgWindowID != window.cgWindowID && screenFrame.contains(target.frame) && !leadingAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
-            leadingAdjacentWindows.append(.init(target))
+        for target in leadingWindows where target.cgWindowID != window.cgWindowID && !leadingAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
+            let itemFrame = target.frame
+            if screenFrame.contains(itemFrame) {
+                let shouldAdjustHeight = abs(itemFrame.minY - frame.minY) < 20 && abs(itemFrame.maxY - frame.maxY) < 20
+                leadingAdjacentWindows.append(.init(target, initialFrame: itemFrame, shouldAlsoAdjustHeight: shouldAdjustHeight))
+            }
         }
 
         // Trailing
         let trailingWindows = windows.filter { $0.frame.minX.approximatelyEquals(to: frame.maxX, tolerance: 100) }
-        for target in trailingWindows where target.cgWindowID != window.cgWindowID && screenFrame.contains(target.frame) && !trailingAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
-            trailingAdjacentWindows.append(.init(target))
+        for target in trailingWindows where target.cgWindowID != window.cgWindowID && !trailingAdjacentWindows.contains(where: { $0.window.cgWindowID == target.cgWindowID }) {
+            let itemFrame = target.frame
+            if screenFrame.contains(itemFrame) {
+                let shouldAdjustHeight = abs(itemFrame.minY - frame.minY) < 20 && abs(itemFrame.maxY - frame.maxY) < 20
+                trailingAdjacentWindows.append(.init(target, initialFrame: itemFrame, shouldAlsoAdjustHeight: shouldAdjustHeight))
+            }
         }
     }
 
     func moveTopAdjacentWindows() {
-        guard let window = mainWindow else { return }
-        let frame = window.frame
+        guard let frame = mainWindow?.frame else { return }
 
         for item in topAdjacentWindows {
-            let newOrigin = CGPoint(x: window.frame.minX, y: item.initialFrame.minY)
-            let newSize = CGSize(width: window.frame.width, height: frame.maxY - frame.height - item.initialFrame.minY - windowSpacing)
+            let shouldAdjustWidth = item.shouldAdjustWidth
+            let newOrigin = CGPoint(x: shouldAdjustWidth ? frame.minX : item.initialFrame.minX, y: item.initialFrame.minY)
+            let newSize = CGSize(width: shouldAdjustWidth ? frame.width : item.initialFrame.width, height: frame.maxY - frame.height - item.initialFrame.minY - windowSpacing)
             item.window.setFrame(.init(origin: newOrigin, size: newSize))
         }
     }
 
     func moveBottomAdjacentWindows() {
-        guard let window = mainWindow else { return }
-        let frame = window.frame
+        guard let frame = mainWindow?.frame else { return }
 
         for item in bottomAdjacentWindows {
-            let newOrigin = window.frame.bottomLeftPoint
-            let newSize = CGSize(width: window.frame.width, height: item.initialFrame.maxY - frame.maxY - windowSpacing)
+            let shouldAdjustWidth = item.shouldAdjustWidth
+            let newOrigin = CGPoint(x: shouldAdjustWidth ? frame.minX : item.initialFrame.minX, y: frame.maxY)
+            let newSize = CGSize(width: shouldAdjustWidth ? frame.width : item.initialFrame.width, height: item.initialFrame.maxY - frame.maxY - windowSpacing)
             item.window.setFrame(.init(origin: newOrigin, size: newSize))
         }
     }
 
     func moveLeadingAdjacentWindows() {
-        guard let window = mainWindow else { return }
-        let frame = window.frame
+        guard let frame = mainWindow?.frame else { return }
 
         for item in leadingAdjacentWindows {
-            let newOrigin = CGPoint(x: item.initialFrame.minX, y: window.frame.minY)
-            let newSize = CGSize(width: frame.maxX - frame.width - item.initialFrame.minX - windowSpacing, height: window.frame.height)
+            let shouldAdjustHeight = item.shouldAdjustHeight
+            let newOrigin = CGPoint(x: item.initialFrame.minX, y: shouldAdjustHeight ? frame.minY : item.initialFrame.minY)
+            let newSize = CGSize(width: frame.maxX - frame.width - item.initialFrame.minX - windowSpacing, height: shouldAdjustHeight ? frame.height : item.initialFrame.height)
             item.window.setFrame(.init(origin: newOrigin, size: newSize))
         }
     }
 
     func moveTrailingAdjacentWindows() {
-        guard let window = mainWindow else { return }
-        let frame = window.frame
+        guard let frame = mainWindow?.frame else { return }
 
         for item in trailingAdjacentWindows {
-            let newOrigin = window.frame.topRightPoint
-            let newSize = CGSize(width: item.initialFrame.maxX - frame.maxX - windowSpacing, height: window.frame.height)
+            let shouldAdjustHeight = item.shouldAdjustHeight
+            let newOrigin = CGPoint(x: frame.maxX, y: shouldAdjustHeight ? frame.minY : item.initialFrame.minY)
+            let newSize = CGSize(width: item.initialFrame.maxX - frame.maxX - windowSpacing, height: shouldAdjustHeight ? frame.height : item.initialFrame.height)
             item.window.setFrame(.init(origin: newOrigin, size: newSize))
         }
     }
