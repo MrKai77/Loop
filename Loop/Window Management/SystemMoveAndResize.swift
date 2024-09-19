b59504bed9998983bd414709e60bc8ed216fd8d1
Kai Azim <hello@mrkai77.dev>
2024-09-19 17:45:19 -0600

    🌐 Use `identifier` so that system resize works in other languages

--- Loop/Window Management/SystemMoveAndResize.swift
+++ Loop/Window Management/SystemMoveAndResize.swift
@@ -11,45 +11,50 @@ import SwiftUI
 @available(macOS 15, *)
 enum SystemMoveAndResize: String {
     // General
-    case minimize = "Minimize"
-    case zoom = "Zoom"
-    case fill = "Fill"
-    case center = "Center"
+    case minimize = "_performMiniaturize:"
+    case zoom = "_performZoom:"
+    case fill = "_zoomFill:"
+    case center = "_zoomCenter:"
+
+    // Halves
+    case left = "_zoomLeft:"
+    case right = "_zoomRight:"
+    case top = "_zoomTop:"
+    case bottom = "_zoomBottom:"
+
+    // Quarters
+    case topLeft = "_zoomTopLeft:"
+    case topRight = "_zoomTopRight:"
+    case bottomLeft = "_zoomBottomLeft:"
+    case bottomRight = "_zoomBottomRight:"
+
+    // Arrange
+    case leftAndRight = "_zoomLeftAndRight:"
+    case rightAndLeft = "_zoomRightAndLeft:"
+    case topAndBottom = "_zoomTopAndBottom:"
+    case bottomAndTop = "_zoomBottomAndTop:"
+    case quarters = "_zoomQuarters:"
+
+    case returnToPreviousSize = "_zoomUntile:"
+
     static var generalActions: [SystemMoveAndResize] {
         [.minimize, .zoom, .fill, .center]
     }
 
-    // Halves
-    case left = "Left"
-    case right = "Right"
-    case top = "Top"
-    case bottom = "Bottom"
     static var halvesActions: [SystemMoveAndResize] {
         [.left, .right, .top, .bottom]
     }
 
-    // Quarters
-    case topLeft = "Top Left"
-    case topRight = "Top Right"
-    case bottomLeft = "Bottom Left"
-    case bottomRight = "Bottom Right"
     static var quartersActions: [SystemMoveAndResize] {
         [.topLeft, .topRight, .bottomLeft, .bottomRight]
     }
 
-    // Arrange
-    case leftAndRight = "Left & Right"
-    case rightAndLeft = "Right & Left"
-    case topAndBottom = "Top & Bottom"
-    case bottomAndTop = "Bottom & Top"
-    case quarters = "Quarters"
     static var arrangeActions: [SystemMoveAndResize] {
         [.leftAndRight, .rightAndLeft, .topAndBottom, .bottomAndTop, .quarters]
     }
 
-    case returnToPreviousSize = "Return to Previous Size"
-
     func perform(on app: NSRunningApplication) {
+        print("Performing system move/resize action")
         do {
             let item = try getItem(for: app)
             try item?.performAction(.press)
@@ -59,28 +64,29 @@ enum SystemMoveAndResize: String {
     }
 
     private func getItem(for app: NSRunningApplication) throws -> AXUIElement? {
-        let moveAndResize = "Move & Resize"
         let pid = app.processIdentifier
 
         // Scan menubar items
         let element = AXUIElementCreateApplication(pid)
-        let menubar: CFTypeRef? = try element.getValue(.menuBar)
-        let menubarItems = (menubar as! AXUIElement).children
-        let windowMenu = try menubarItems.first {
-            try $0.getValue(.title) == "Window"
-        }
+        let menubar = try (element.getValue(.menuBar) as CFTypeRef?) as! AXUIElement
+        let menubarItems = menubar.children.reversed() // Help menu will be last
 
-        if SystemMoveAndResize.generalActions.contains(self) {
-            let moveAndResizeItems = windowMenu?.children.first?.children
-            return try moveAndResizeItems?
-                .first(where: { try $0.getValue(.title) == rawValue }) ?? moveAndResizeItems?
-                .first(where: { try ($0.getValue(.title) as String?)?.localizedCaseInsensitiveContains(rawValue) ?? false })
-        } else {
-            if let items = windowMenu?.children.first?.children,
-               let resizeItems = try items.first(where: { try $0.getValue(.title) == moveAndResize })?.children.first?.children {
-                return try resizeItems
-                    .first(where: { try $0.getValue(.title) == rawValue }) ?? resizeItems
-                    .first(where: { try ($0.getValue(.title) as String?)?.localizedCaseInsensitiveContains(rawValue) ?? false })
+        for menubarItem in menubarItems {
+            guard let windowMenuItems = menubarItem.children.first?.children else {
+                continue
+            }
+
+            if SystemMoveAndResize.generalActions.contains(self),
+               let menuItem = try windowMenuItems.first(where: { try $0.getValue(.identifier) == rawValue }) {
+                return menuItem
+            } else {
+                let menuItemsWithSubmenu = windowMenuItems.filter { $0.children.first?.children != nil }.map(\.children.first)
+
+                for item in menuItemsWithSubmenu {
+                    if let menuItem = try item?.children.first(where: { try $0.getValue(.identifier) as String? == rawValue }) {
+                        return menuItem
+                    }
+                }
             }
         }
 
