d86bd5a12875463eb9c2647dffc8a667feb07388
Kami <kamibusinessinquirers@gmail.com>
2025-01-04 21:29:47 +1000

    🐞 Fix loop crash

--- Loop/Utilities/EventMonitor.swift
+++ Loop/Utilities/EventMonitor.swift
@@ -30,7 +30,13 @@ class NSEventMonitor: EventMonitor, Identifiable, Equatable {
     }
 
     deinit {
-        stop()
+        if isEnabled {
+            stop()
+        }
+
+        // Clear references
+        localEventMonitor = nil
+        globalEventMonitor = nil
     }
 
     init(scope: Scope, eventMask: NSEvent.EventTypeMask, handler: @escaping (NSEvent) -> (NSEvent?)) {
@@ -62,12 +68,16 @@ class NSEventMonitor: EventMonitor, Identifiable, Equatable {
     }
 
     func stop() {
+        guard isEnabled else { return }
+
         if let localEventMonitor {
             NSEvent.removeMonitor(localEventMonitor)
+            self.localEventMonitor = nil
         }
 
         if let globalEventMonitor {
             NSEvent.removeMonitor(globalEventMonitor)
+            self.globalEventMonitor = nil
         }
         isEnabled = false
     }
@@ -110,7 +120,19 @@ class CGEventMonitor: EventMonitor, Identifiable, Equatable {
     }
 
     deinit {
-        stop()
+        if isEnabled {
+            stop()
+        }
+
+        // Clean up run loop source and event tap
+        if let runLoopSource {
+            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            self.runLoopSource = nil
+        }
+
+        if eventTap != nil {
+            self.eventTap = nil
+        }
     }
 
     private func handleEvent(event: CGEvent) -> Unmanaged<CGEvent>? {
@@ -118,16 +140,16 @@ class CGEventMonitor: EventMonitor, Identifiable, Equatable {
     }
 
     func start() {
-        if let eventTap {
-            CGEvent.tapEnable(tap: eventTap, enable: true)
-        }
+        guard let eventTap else { return }
+        CGEvent.tapEnable(tap: eventTap, enable: true)
         isEnabled = true
     }
 
     func stop() {
-        if let eventTap {
-            CGEvent.tapEnable(tap: eventTap, enable: false)
-        }
+        guard isEnabled else { return }
+
+        guard let eventTap else { return }
+        CGEvent.tapEnable(tap: eventTap, enable: false)
         isEnabled = false
     }
 
