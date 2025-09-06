28f9d76db5c1cd4535a63f00c9871f120176deef
Cipher Shad0w <jannis.krija@icloud.com>
2025-09-06 11:50:07 +0200

    ✨ Fix method call for minimizing windows to ensure correct functionality

--- Loop/Window Management/WindowEngine.swift
+++ Loop/Window Management/WindowEngine.swift
@@ -39,7 +39,7 @@ enum WindowEngine {
         }
 
         if action.direction == .minimizeOthers {
-            minimizeOthersWindows(exceptWindow: window)
+            minimizeOtherWindows(exceptWindow: window)
             return
         }
 
