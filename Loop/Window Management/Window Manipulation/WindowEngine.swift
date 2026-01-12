a9969463f18ebc7d43386fdbc8e0c699371a6caf
Kai Azim <hi@mrkai77.dev>
2026-01-11 19:51:43 -0700

    ✨ Store actions into WindowRecords beforehand if possible

--- Loop/Window Management/Window Manipulation/WindowEngine.swift
+++ Loop/Window Management/Window Manipulation/WindowEngine.swift
@@ -56,11 +56,18 @@ enum WindowEngine {
         // Record first frame if needed
         WindowRecords.recordFirstIfNeeded(for: window)
 
-        // Defer recording action or undo
+        let storeAsFrame = WindowRecords.shouldStoreAsFinalFrame(action)
+        
+        /// If this action doesn't require storage as a frame, then record it beforehand.
+        /// Otherwise, this action will be recorded *after* resizing, such that its final frame is considered if undoing.
+        if !storeAsFrame {
+            WindowRecords.record(window, action)
+        }
+        
         defer {
             if action.direction == .undo {
                 WindowRecords.removeLastAction(for: window)
-            } else {
+            } else if storeAsFrame {
                 WindowRecords.record(window, action)
             }
         }
